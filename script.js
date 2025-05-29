514975c72c4b5653e2af0232e9dd75d30080e8ed
Mark <marklajom.work@gmail.com>
2025-05-29 17:50:47 +0200

    add initial styling

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ function getAllContacts() {
   return document.querySelectorAll(".contact");
 }
 
+const oLStickyNoteSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!-- COMMENT --!><path d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l224 0 0-80c0-17.7 14.3-32 32-32l80 0 0-224c0-8.8-7.2-16-16-16L64 80zM288 480L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 224 0 5.5c0 17-6.7 33.3-18.7 45.3l-90.5 90.5c-12 12-28.3 18.7-45.3 18.7l-5.5 0z"/></svg>`;
+const oLHeartSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>`;
+const oLEditSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg>`;
+const removeSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>`;
+const checkSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>`;
+const solidHeartSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/></svg>`;
+
 const currentContacts = getAllContacts();
 
 let contactArray = [
@@ -18,7 +25,7 @@ let contactArray = [
     backgroundColor: "#FF6B6B",
     firstName: "Alice",
     lastName: "Johnson",
-    birthday: "1995-06-12",
+    birthdate: "1995-06-12",
     lastContact: "2024-12-01",
     notes: "OSKABOLD ",
     isFavourite: false,
@@ -28,7 +35,7 @@ let contactArray = [
     backgroundColor: "#6BCB77",
     firstName: "Ben",
     lastName: "Nguyen",
-    birthday: "1990-03-28",
+    birthdate: "1990-03-28",
     lastContact: "2025-02-20",
     notes: "",
     isFavourite: false,
@@ -38,7 +45,7 @@ let contactArray = [
     backgroundColor: "#4D96FF",
     firstName: "Carla",
     lastName: "Smith",
-    birthday: "1987-11-05",
+    birthdate: "1987-11-05",
     lastContact: "2025-04-10",
     notes: "",
     isFavourite: false,
@@ -48,7 +55,7 @@ let contactArray = [
     backgroundColor: "#FFD93D",
     firstName: "David",
     lastName: "Lee",
-    birthday: "2001-08-22",
+    birthdate: "2001-08-22",
     lastContact: "2025-01-15",
     notes: "",
     isFavourite: false,
@@ -58,7 +65,7 @@ let contactArray = [
     backgroundColor: "#9D4EDD",
     firstName: "Ella",
     lastName: "Martinez",
-    birthday: "1998-02-17",
+    birthdate: "1998-02-17",
     lastContact: "2025-03-01",
     notes: "",
     isFavourite: false,
@@ -79,7 +86,7 @@ function addContact(e) {
         addContactForm.backgroundColor.value || "#ffffff";
       editContact.firstName = addContactForm.firstName.value;
       editContact.lastName = addContactForm.lastName.value;
-      editContact.birthday = addContactForm.birthday.value;
+      editContact.birthdate = addContactForm.birthdate.value;
       editContact.lastContact = addContactForm.lastContact.value;
       editContact.notes = addContactForm.notes.value;
     }
@@ -91,7 +98,7 @@ function addContact(e) {
       backgroundColor: addContactForm.backgroundColor.value || "#ffffff",
       firstName: addContactForm.firstName.value,
       lastName: addContactForm.lastName.value,
-      birthday: addContactForm.birthday.value,
+      birthdate: addContactForm.birthdate.value,
       lastContact: addContactForm.lastContact.value,
       notes: addContactForm.notes.value,
       isFavourite: false,
@@ -114,23 +121,23 @@ function populateList(contacts) {
                         }">
                           <span>${contact.firstName.charAt(0)}</span>
                         </div>
-                        <div>
+                        <div class="personal-info">
                           <span>${contact.firstName} ${contact.lastName}</span>
-                          <span>Birthday: ${contact.birthday}</span>
+                          <span>Birthdate: ${contact.birthdate}</span>
                         </div>
                       </div>
                       <div class="contact-options">
                         <div class="last-contact">
                        <span>Last Contacted: ${contact.lastContact}</span>
                         
-                          <button class="update-today">✅</button>
+                          <button class="update-today">${checkSVG}</button>
                         </div>
                            <div class="contact-edit">
-                          <button class="notes-contact">note</button>
+                          <button class="notes-contact">${oLStickyNoteSVG}</button>
                           <div>
-                            <button class="favourite">fav</button>
-                            <button class="edit-contact">edit</button>
-                            <button class="delete-contact">remove</button>
+                            <button class="favourite">${oLHeartSVG}</button>
+                            <button class="edit-contact">${oLEditSVG}</button>
+                            <button class="delete-contact">${removeSVG}</button>
                           </div>
                         </div>
                       </div>
@@ -141,29 +148,37 @@ function populateList(contacts) {
 
 function handleClick(e) {
   const contactElement = e.target.closest(".contact");
+
+  if (!contactElement) return;
+
   const contactId = parseInt(contactElement.id, 10);
   const contact = contactArray.find((contact) => contact.id === contactId);
+  const favouriteBtn = e.target.closest(".favourite");
+  const editBtn = e.target.closest(".edit-contact");
+  const deleteBtn = e.target.closest(".delete-contact");
+  const updateTodayBtn = e.target.closest(".update-today");
+  const notesBtn = e.target.closest(".notes-contact");
 
-  if (e.target.classList.contains("favourite")) {
+  if (favouriteBtn) {
     contact.isFavourite = !contact.isFavourite;
-    e.target.textContent = contact.isFavourite ? "❤️" : "fav";
-  } else if (e.target.classList.contains("edit-contact")) {
+    e.target.innerHTML = contact.isFavourite ? solidHeartSVG : oLHeartSVG;
+  } else if (editBtn) {
     modal.showModal();
 
     addContactForm.backgroundColor.value = contact.backgroundColor;
     addContactForm.firstName.value = contact.firstName;
     addContactForm.lastName.value = contact.lastName;
-    addContactForm.birthday.value = contact.birthday;
+    addContactForm.birthdate.value = contact.birthdate;
     addContactForm.lastContact.value = contact.lastContact;
     addContactForm.notes.value = contact.notes;
 
     addContactForm.dataset.editingId = contact.id;
-  } else if (e.target.classList.contains("delete-contact")) {
+  } else if (deleteBtn) {
     if (window.confirm("Do you really want to delete this contact?")) {
       contactArray = contactArray.filter((contact) => contact.id !== contactId);
       contactElement.remove();
     } else return;
-  } else if (e.target.classList.contains("update-today")) {
+  } else if (updateTodayBtn) {
     const today = new Date();
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, "0");
@@ -178,7 +193,7 @@ function handleClick(e) {
         dateSpan.textContent = `Last Contacted: ${formattedDate}`;
       }
     }
-  } else if (e.target.classList.contains("notes-contact")) {
+  } else if (notesBtn) {
     notesModal.showModal();
 
     addNotesForm.notesX.value = contact.notes;

6bb1c9d65ae15e3b3898144572f9acb714f51d13
Mark <marklajom.work@gmail.com>
2025-06-03 16:56:10 +0200

    add sort by favourite

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const addNotesForm = document.getElementById("add-notes-form");
 const contactList = document.querySelector(".contact-list");
 const notesModal = document.getElementById("notes-modal");
 const closeNotesButton = document.getElementById("close-notes-modal");
+const contactSortSelect = document.getElementById("contact-sort");
 
 const oLStickyNoteSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M64 80c-8.8 0-16 7.2-16 16l0 320c0 8.8 7.2 16 16 16l224 0 0-80c0-17.7 14.3-32 32-32l80 0 0-224c0-8.8-7.2-16-16-16L64 80zM288 480L64 480c-35.3 0-64-28.7-64-64L0 96C0 60.7 28.7 32 64 32l320 0c35.3 0 64 28.7 64 64l0 224 0 5.5c0 17-6.7 33.3-18.7 45.3l-90.5 90.5c-12 12-28.3 18.7-45.3 18.7l-5.5 0z"/></svg>`;
 const oLHeartSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8l0-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5l0 3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20-.1-.1s0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5l0 3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2l0-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>`;
@@ -180,6 +181,38 @@ function addNotes(e) {
   notesModal.close();
 }
 
+function sortContactArray(e) {
+  let sortOption = e.target.value;
+  let sortedArray = contactArray;
+
+  console.log(sortedArray);
+  if (sortOption === "favourite") {
+    sortedArray.sort((a, b) => {
+      if (a.isFavourite && !b.isFavourite) {
+        return -1;
+      } else if (!a.isFavourite && b.isFavourite) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    populateList(sortedArray);
+  } else if (sortOption === "name-asc") {
+    console.log(sortOption);
+  } else if (sortOption === "name-desc") {
+    console.log(sortOption);
+  } else if (sortOption === "lastContact-asc") {
+    console.log(sortOption);
+  } else if (sortOption === "lastContact-desc") {
+    console.log(sortOption);
+  } else if (sortOption === "birthdate-asc") {
+    console.log(sortOption);
+  } else if (sortOption === "birthdate-desc") {
+    console.log(sortOption);
+  } else populateList(contactArray);
+}
+
 populateList(contactArray);
 contactList.addEventListener("click", handleClick);
 addContactForm.addEventListener("submit", addContact);
@@ -187,3 +220,4 @@ notesModal.addEventListener("submit", addNotes);
 closeNotesButton.addEventListener("click", () => notesModal.close());
 openButton.addEventListener("click", () => modal.showModal());
 closeButton.addEventListener("click", () => modal.close());
+contactSortSelect.addEventListener("change", sortContactArray);
